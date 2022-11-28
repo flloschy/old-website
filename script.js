66d73c504bee89846c0de4510252eef9574c5737
flloschy <floschy.email@gmail.com>
2022-11-28 21:15:28 +0100

    Awsome new content (not)

--- script.js
+++ script.js
@@ -1,119 +1,187 @@
 
-window.onload = function() {
+const randomWebsite = function() {
     
     let links = [
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://downloadramdownloadramdownloadram.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://papertoilet.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://codepen.io/ge1doot/full/WbWQOP/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://trypap.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://isitchristmas.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.ouaismaisbon.ch/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.koalastothemax.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://make-everything-ok.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://rulesoftheinternet.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://ninjaflex.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://internet-map.net/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.plspetdoge.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://faceofdisapproval.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.kamogo.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://eelslap.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.freechocolate.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.woot.co.uk/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.howmanypeopleareinspacerightnow.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://hasthelargehadroncolliderdestroyedtheworldyet.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.cowsgomoo.co.uk/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.clicktoremove.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.tomscott.com/usvsth3m/maths/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://nothingeverhappens.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://drumsound.net/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://froggytime.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.youareinaforest.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://clickingbad.nullism.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://onetinyhand.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://corndogoncorndog.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.omfgdogs.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.sheepfilms.co.uk/gifs?pgid=kctc5nrd-afad6bc9-f898-4c06-83df-4ebd2de83b0f",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://www.rrrather.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "https://bouncingdvdlogo.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://www.cachemonet.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://randomcolour.com/",
-        "https://youtu.be/dQw4w9WgXcQ",
         "http://kryall.duckdns.org:5000/",
         "http://www.slightlyinteresting.com/lavalamp/lava.asp",
         "https://youtu.be/dQw4w9WgXcQ",
-        "http://kryall.duckdns.org:5000/",
         "http://conceptlab.com/simulator/morning/clock800.html",
-        "https://youtu.be/dQw4w9WgXcQ"
+        "https://tse4.mm.bing.net/th?id=OIP.OvVtjJ31jDHzKZC2M-SkPQHaEK&pid=Api",
+        "https://www.google.com/fbx?fbx=snake_arcade",
+        "https://www.google.com/fbx?fbx=minesweeper",
     ]
     window.location.href = links[Math.floor(Math.random() * links.length)];
+}
+
+const DecimalToBinary = function(x) {
+    let out = ""
+    x = parseInt(x, 10)
+    while (x != 0) {
+        out = (x % 2) + out
+        x -= Math.ceil(x / 2)
+    }
+    alert("Binary: " + out)
+}
+
+const DecimalToHexadecimal = function(x) {
+    x = parseInt(x, 10);
+    let out = x.toString(16).toUpperCase();
+    alert("Hexadecimal: " + out)
+}
+
+const HexadecimalToDecimal = function(x) {
+    x = parseInt(x, 16);
+    alert("Decimal: " + x)
+}
+
+const BinaryToDecimal = function(x) {
+    x = parseInt(x, 2)
+    alert("Decimal: " + x)
+}
+
+const checkBinaryKey = function() {
+    let key = String.fromCharCode(window.event.keyCode);
+    return key == "1" || key == "0";
+}
+
+const checkNumberKey = function() {
+    let key = String.fromCharCode(window.event.keyCode);
+    let keys = "0123456789".split("")
+    return keys.includes(key);
+}
+
+const checkHexaNumberKey = function() {
+    let key = String.fromCharCode(window.event.keyCode);
+    let keys = "0123456789abcdef".split("")
+    return keys.includes(key.toLowerCase()) ;
+}
+
+const randomPwd = function() {
+    let len = 15;
+    let password = "";
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/()=?`{[]}*><|@".split("")
+    for (let i = 0; i < len; i++) {
+        password = password + chars[Math.floor(Math.random() * chars.length)]
+    }
+    alert(password)
+}
+
+const checkPwdkey = function() {
+    let keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!§$%&/()=?`{[]}*><|@".split("")
+    let key = String.fromCharCode(window.event.keyCode);
+    return keys.includes(key)
+}
+
+const PwdCheck = function(x) {
+    let numbers = "0123456789".split("")
+    let chars = "abcdefghijklmnopqrstuvwxy".split("")
+    let charsUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    let special = "!§$%&/()=?`{[]}*><|@".split("")
+    
+    let percentage = 0;
+
+    if (x.length > 100) {
+        percentage += 70;
+    } else if (x.length > 60) {
+        percentage += 50;
+    } else if (x.length > 40) {
+        percentage += 30;
+    } else if (x.length > 20) {
+        percentage += 5;
+    } else if (x.length > 10) {
+        percentage += 2;
+    } else {
+        percentage -= 10
+    }
+
+
+    let n = 0
+    let c = 0
+    let c2 = 0
+    let s = 0
+
+    x.split("").forEach(e => {
+        if (numbers.includes(e)) n += 1
+        else if (chars.includes(e)) c += 1
+        else if (charsUp.includes(e)) c2 += 1
+        else if (special.includes(e)) s += 1
+    })
+
+    let t = x.length
+
+    n = Math.round(n / t * 100)
+    c = Math.round(c / t * 100)
+    c2 = Math.round(c2 / t * 100) 
+    s = Math.round(s / t * 100)
+
+    if (n < 5) percentage -= 10;
+    if (c < 5) percentage -= 10;
+    if (c2 < 5) percentage -= 10;
+    if (s < 5) percentage -= 10;
+    
+    if (n > 10) percentage += 5;
+    if (c > 10) percentage += 5;
+    if (c2 > 10) percentage += 5;
+    if (s > 10) percentage += 5;
+
+    if (n > 15) percentage += 5;
+    if (c > 15) percentage += 5;
+    if (c2 > 15) percentage += 5;
+    if (s > 15) percentage += 5;
+
+    if (n > 20) percentage += 5;
+    if (c > 20) percentage += 5;
+    if (c2 > 20) percentage += 5;
+    if (s > 20) percentage += 5;
+
+    if (n > 25) percentage += 5;
+    if (c > 25) percentage += 5;
+    if (c2 > 25) percentage += 5;
+    if (s > 25) percentage += 5;
+
+
+    if (percentage == 130) alert("(9/9) Password is perfecto")
+    else if (percentage > 100) alert("(8/9) Password is gud")
+    else if (percentage > 80) alert("(7/9) Password is ok")
+    else if (percentage > 60) alert("(6/9) Password is meh")
+    else if (percentage > 40) alert("(5/9) Password is irgh")
+    else if (percentage > 20) alert("(4/9) Password is ...")
+    else if (percentage == 0) alert("(3/9) Password is shit")
+    else if (percentage > -20) alert("(2/9) Password is extremely stupid")
+    else alert("(1/9) Password is absolutely dumb, please think about your life decisions!")
 }
