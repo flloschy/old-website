78bce13ae793c61fcbbc5dd4efa1b52133ed95d6
flloschy <floschy.email@gmail.com>
2022-11-30 23:21:16 +0100

    Links! WOW!

--- src/js/links.js
+++ src/js/links.js
@@ -1,33 +1,36 @@
-const getRandomPosition = function(element, used) {
-    let ret = false
-    let randomX = 0
-    let randomY = 0;
-    let space = 50
-    while (!ret) {
-        let y = document.body.offsetHeight - element.clientHeight - 24 - space;
-        let x = document.body.offsetWidth - element.clientWidth - space;
-        randomX = Math.floor(Math.random()*x) + Math.round(space/2);
-        randomY = Math.floor(Math.random()*y) + 24 + Math.round(space/2);
-        
-        ret = !used || used.every(e => (
-            ( e[3] < (randomY - space) ) ||
-            ( e[1] > (randomY + element.clientHeight + space)) ||
-            ( e[2] < (randomX - space) ) ||
-            ( e[0] > (randomX +element.clientWidth + space))
-        ))
-            
-    }
-    return [randomX, randomY]
-}
+let el = 1;
+let inters = null;
+let amount = 5
+
 const updatePos = function() {
-    let used = []
-    for (let i = 0; i < 3; i++) {
+
+    let radius = document.body.clientWidth < document.body.clientHeight ? document.body.clientWidth * 0.4 : document.body.clientHeight *0.4
+    radius = Math.round(radius)
+
+    for (var i = 0; i < amount; i++) {
+        let offsetAngle = 360 / amount;
+        let rotateAngle = offsetAngle * (i+1) + offsetAngle * el;
         let link = document.getElementById(`link${i+1}`)
-        console.log(i);
-        link.innerHTML
-        let [x, y] = getRandomPosition(link, used)
-        link.style.top = y + "px"
-        link.style.left = x + "px"
-        used.push([x, y, x+link.clientWidth, y+link.clientHeight])
+        try {link.innerHTML}
+        catch (e) {pauseInverval()}
+        link.style.transform = "rotate(" + rotateAngle + "deg) translate(0, -" + radius + "px)"
+        link.style.top = document.body.clientHeight / 2
+        let lastlink = document.getElementById(`link${i == 0 ? amount : i}`)
+        o = Math.pow(Math.pow(1 - link.getBoundingClientRect().y / document.body.clientHeight, 8), 2) * 10;
+        lastlink.style.opacity = o
+    };
+
+    el += 1
+}
+
+const interval = function() {
+    if (inters == null) {
+        updatePos()
+        inters = setInterval(updatePos, 2500)
     }
+}
+
+const pauseInverval = function() {
+    clearInterval(inters)
+    inters = null
 }
