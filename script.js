fc0469e9d5967325e80136d6273bb63c2ea249d0
flloschy <floschy.email@gmail.com>
2023-02-24 20:09:08 +0100

    cool stuff

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const textList = [
+    {text: "welcome to my website", link: "https://floschy.me/"},
+    {text: "floschy.me", link: "https://floschy.me/"},
+    {text: "github.com/flloschy", link: "https://github.com/flloschy"},
+    {text: "floschy#1248", link: "https://discord.com/users/578620425060483072"},
+]
+let current = textList.length - 1;
+let temptext = textList[current].text.split("");
+let id = undefined;
 var ring, block;
-var x = 0;
-var y = 0;
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
 var moving = false;
 
 function updateCursor() {
-    ring.style.opacity = 1;
+    moving = true;
+    
     block.style.filter =
         "blur(150px) hue-rotate(" +
         Math.floor((y + window.scrollY) / window.innerHeight) * 18 +
@@ -23,7 +33,7 @@ function updateCursor() {
             left: x - block.clientWidth / 2 + "px",
             top: y - block.clientHeight / 2 + window.scrollY + "px",
         },
-        { duration: 2000, fill: "forwards", easing: "ease-in-out" }
+        { duration: 6000, fill: "forwards", easing: "ease-in-out" }
     );
 }
 
@@ -31,11 +41,10 @@ function itsDone() {
     block = document.getElementById("block");
     ring = document.getElementById("ring");
 
-    document.addEventListener("mousemove", function (event) {
-        moving = true;
-        x = event.clientX;
-        y = event.clientY;
+    updateCursor();
 
+    document.addEventListener("mousemove", function (event) {
+        [x, y] = [event.clientX, event.clientY];
         updateCursor();
     });
 
@@ -45,8 +54,9 @@ function itsDone() {
 
     setInterval(function () {
         if (!moving) {
-            normit();
             ring.style.opacity = 0;
+            block.style.opacity = .5;
+
             const randomX =
                 Math.floor((Math.random() * window.outerWidth) / 4) -
                 window.outerWidth / 8;
@@ -67,75 +77,70 @@ function itsDone() {
             );
         } else {
             moving = false;
+            ring.style.opacity = 1;
+            block.style.opacity = 1;
         }
     }, 2500);
-}
-const originalText = "floschy.me";
-let text = originalText.split("");
-
-let id = setInterval(function () {
-    if (!text.length) {
-        clearInterval(id);
-        return;
-    }
-    let title = document.getElementById("title");
-    title.innerHTML += text.shift();
-}, 100);
-
-let Rid = undefined;
-let Nid = undefined;
-let i = 0;
-let u = 0;
-function randomize() {
-    if (Nid) clearInterval(Nid);
-
-    let title = document.getElementById("title");
-    i = 0;
-    Rid = setInterval(function () {
-        if (i == title.innerHTML.length) {
-            i = 0;
-            u += 1;
-            if (u == 3) {
-                u = 0;
-                clearInterval(Rid);
-                Rid = undefined;
-                return;
-            }
-        }
-        let text2 = title.innerHTML.split("");
-        text2[i] = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-        text2[Math.floor(Math.random() * title.innerHTML.length)] =
-            String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-        title.innerHTML = text2.join("");
-
-        i++;
-    }, 50);
+    toggle()
 }
 
-function normit() {
-    if (Rid) clearInterval(Rid);
+
+
+
+
+
+
+///////////////////////////////////////////////////
+
+
+
+
+function toggle() {
+    if (id) return;
+    current = (current + 1) % textList.length;
+    
+    let i1 = 0;
+
+    let randomstring = textList[current].text.split("").map((v, i, a) => {return {text:v, index:i}}).sort(e => Math.random()-.5);
 
     let title = document.getElementById("title");
-    i = 0;
-    Nid = setInterval(function () {
-        if (i == title.innerHTML.length) {
-            i = 0;
-            u += 1;
-            if (u == 3) {
-                u = 0;
-                clearInterval(Nid);
-                Nid = undefined;
-                return;
+
+    id = setInterval(function () {
+        if (i1 < textList[current].text.length) {
+            temptext = title.innerHTML.split("");
+            
+            if (title.innerHTML.length > textList[current].text.length) {
+                temptext.pop();
+            }
+            else if (textList[current].text.length > title.innerHTML.length) {
+                temptext.push("");
+            }
+            temptext[i1] = randomstring[i1].text;
+
+            title.innerHTML = temptext.join("");
+            i1++;
+        } else {
+
+            let sorted = true;
+            for (let i = 0; i < randomstring.length-1; i++) {
+                if (randomstring[i].index > randomstring[i+1].index) {
+                    let temp = randomstring[i];
+                    randomstring[i] = randomstring[i+1];
+                    randomstring[i+1] = temp;
+                    sorted = false;
+                }
+            }
+
+
+            
+
+            title.innerHTML = randomstring.map(e => e.text).join("");
+
+            if (sorted) {
+                clearInterval(id);
+                title.href = textList[current].link;
+                id = undefined;
             }
         }
-        let text2 = title.innerHTML.split("");
-        if (u == 2) {
-            text2[i] = originalText.split("")[i];
-        }
-        let r = Math.floor(Math.random() * title.innerHTML.length);
-        text2[r] = originalText.split("")[r];
-        title.innerHTML = text2.join("");
-
-        i++;
-    }, 50);
+    }, 100);
 }
